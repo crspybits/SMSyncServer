401438a17cee59d9d26a7c4400c7ac7f2f40c1cd
Christopher Prince <chris@SpasticMuffin.biz>
2016-07-17 22:35:30 -0600

    Continued development of sharing tests.

--- iOS/iOSTests/XCTests/SharingUserOperations.swift
+++ iOS/iOSTests/XCTests/SharingUserOperations.swift
@@ -9,6 +9,7 @@
 import XCTest
 import SMCoreLib
 @testable import SMSyncServer
+@testable import Tests
 
 class SharingUserOperations: BaseClass {
 
@@ -75,17 +76,56 @@ class SharingUserOperations: BaseClass {
         }
     }
     
+    var uploadFile1:TestFile!
+    var uploadFile2:TestFile!
+    let uploadFile1UUID = SMPersistItemString(name: "SharingUserOperations.uploadFile1", initialStringValue: "", persistType: .UserDefaults)
+    let uploadFile2UUID = SMPersistItemString(name: "SharingUserOperations.uploadFile2", initialStringValue: "", persistType: .UserDefaults)
+    
+    func createUploadFiles(initial initial:Bool) {
+        if initial {
+            self.uploadFile1 = TestBasics.session.createTestFile("FileDownloadByDownloadSharingUser")
+            self.uploadFile1UUID.stringValue = self.uploadFile1.uuidString
+            
+            self.uploadFile2 = TestBasics.session.createTestFile("DownloadDeletionByDownloadSharingUser")
+            self.uploadFile2UUID.stringValue = self.uploadFile2.uuidString
+        }
+        else {
+            self.uploadFile1 = TestBasics.session.recreateTestFile(fromUUID: self.uploadFile1UUID.stringValue)
+            self.uploadFile2 = TestBasics.session.recreateTestFile(fromUUID: self.uploadFile2UUID.stringValue)
+        }
+    }
+
     // Do this before any of the following tests.
-    // Must be signed in as owning user or sharing user with invite capability
-    func testCreateInvitations() {
-        let createSharingInvitationDone = self.expectationWithDescription("Done Creating")
+    // Must be signed in as Owning User.
+    func testSetupRemainingTests() {
+        let setupDone = self.expectationWithDescription("Done Setup")
         self.extraServerResponseTime = Double(self.numberInvitationsPerType) * 3 * 20
+        let uploadExpectations1 = UploadFileExpectations(fromTestClass: self)
+        let uploadExpectations2 = UploadFileExpectations(fromTestClass: self)
+        let uploadDeletionExpectations = UploadDeletionExpectations(fromTestClass: self)
+       
+        self.createUploadFiles(initial:true)
         
         self.waitUntilSyncServerUserSignin() {
             self.createDownloadingInvitations() {
                 self.createUploadingInvitations() {
                     self.createAdminInvitations() {
-                        createSharingInvitationDone.fulfill()
+                    
+                        SMServerAPI.session.createSharingInvitation(sharingType: SMSharingType.Downloader.rawValue, completion: { (invitationCode, apiResult) in
+                            XCTAssert(apiResult.error == nil)
+                            XCTAssert(invitationCode != nil)
+                            
+                            // So I can sign in as a sharing user below.
+                            AppDelegate.sharingInvitationCode = invitationCode!
+                            
+                            self.uploadFile(self.uploadFile1, expectations: uploadExpectations1) {
+                                self.uploadFile(self.uploadFile2, expectations: uploadExpectations2) {
+                                    self.uploadDeletion(self.uploadFile2, expectation: uploadDeletionExpectations) {
+                                        setupDone.fulfill()
+                                    }
+                                }
+                            }
+                        })
                     }
                 }
             }
@@ -93,64 +133,239 @@ class SharingUserOperations: BaseClass {
     
         self.waitForExpectations()
     }
-    
-    func uploadFileExpectingFailure() {
+ 
+    class UploadFileExpectations {
+        var uploadCompleteCallbackExpectation:XCTestExpectation
+        var singleUploadExpectation:XCTestExpectation
+        var idleExpectation:XCTestExpectation
+        
+        init(fromTestClass testClass:XCTestCase) {
+            self.uploadCompleteCallbackExpectation = testClass.expectationWithDescription("Commit Complete")
+            self.singleUploadExpectation = testClass.expectationWithDescription("Upload Complete")
+            self.idleExpectation = testClass.expectationWithDescription("Idle")
+        }
     }
     
-    /*
-    func uploadFiles(testFiles:[TestFile], uploadExpectations:[XCTestExpectation]?, commitComplete:XCTestExpectation?, idleExpectation:XCTestExpectation,
-        complete:(()->())?) {
+    class DownloadFileExpectations {
+        var singleDownloadExpectation:XCTestExpectation
+        var allDownloadsCompleteExpectation:XCTestExpectation
+        var idleExpectation:XCTestExpectation
         
-        for testFileIndex in 0...testFiles.count-1 {
-            let testFile = testFiles[testFileIndex]
-            let uploadExpectation:XCTestExpectation? = uploadExpectations?[testFileIndex]
+        init(fromTestClass testClass:XCTestCase) {
+            self.singleDownloadExpectation = testClass.expectationWithDescription("Single Download")
+            self.allDownloadsCompleteExpectation = testClass.expectationWithDescription("All Downloads Complete")
+            self.idleExpectation = testClass.expectationWithDescription("Idl1")
+        }
+    }
+    
+    class UploadDeletionExpectations {
+        var deletionExpectation:XCTestExpectation
+        var idleExpectation:XCTestExpectation
+        var commitCompleteExpectation:XCTestExpectation
         
-            try! SMSyncServer.session.uploadImmutableFile(testFile.url, withFileAttributes: testFile.attr)
-            
-            if uploadExpectation != nil {
-                self.singleUploadCallbacks.append() { uuid in
-                    XCTAssert(uuid.UUIDString == testFile.uuidString)
-                    uploadExpectation!.fulfill()
+        init(fromTestClass testClass:XCTestCase) {
+            self.deletionExpectation = testClass.expectationWithDescription("Deletion")
+            self.idleExpectation = testClass.expectationWithDescription("Idle Complete")
+            self.commitCompleteExpectation = testClass.expectationWithDescription("Commit")
+        }
+    }
+    
+    class DownloadDeletionExpectations {
+        var clientShouldDeleteFilesExpectation:XCTestExpectation
+        var idle:XCTestExpectation
+        
+        init(fromTestClass testClass:XCTestCase) {
+            self.clientShouldDeleteFilesExpectation = testClass.expectationWithDescription("Deletion")
+            self.idle = testClass.expectationWithDescription("Idle Complete")
+        }
+    }
+    
+    func uploadFile(testFile:TestFile, expectations:UploadFileExpectations, failureExpected:Bool=false, complete:(()->())?=nil) {
+        try! SMSyncServer.session.uploadImmutableFile(testFile.url, withFileAttributes: testFile.attr)
+        
+        self.singleUploadCallbacks.append() { uuid in
+            XCTAssert(uuid.UUIDString == testFile.uuidString)
+            expectations.singleUploadExpectation.fulfill()
+        }
+        
+        self.idleCallbacks.append() {
+            expectations.idleExpectation.fulfill()
+        }
+        
+        if failureExpected {
+            self.errorCallbacks.append() {
+                SMSyncServer.session.cleanupFile(testFile.uuid)
+                CoreData.sessionNamed(CoreDataTests.name).removeObject(
+                    testFile.appFile)
+                CoreData.sessionNamed(CoreDataTests.name).saveContext()
+                
+                SMSyncServer.session.resetFromError() { error in
+                    XCTAssert(error == nil)
+                    expectations.uploadCompleteCallbackExpectation.fulfill()
                 }
             }
         }
-
-        // The .Idle callback gets called first
-        self.idleCallbacks.append() {
-            if commitComplete == nil {
-                complete?()
-            }
-            idleExpectation.fulfill()
-        }
-        
-        if commitComplete != nil {
-            // Followed by the commit complete callback.
+        else {
             self.commitCompleteCallbacks.append() { numberUploads in
-                XCTAssert(numberUploads == testFiles.count)
-                commitComplete!.fulfill()
-                self.checkFileSizes(testFiles, complete: complete)
+                XCTAssert(numberUploads == 1)
+                TestBasics.session.checkFileSize(testFile.uuidString, size: testFile.sizeInBytes) {
+                    expectations.uploadCompleteCallbackExpectation.fulfill()
+                    complete?()
+                }
             }
         }
         
         try! SMSyncServer.session.commit()
     }
-    */
     
-    // For the rest: Must be signed in as sharing user.
+    func downloadFile(testFile:TestFile, expectations:DownloadFileExpectations,
+        complete:(()->())?=nil) {
+        var numberDownloads = 0
+        
+        SMSyncServer.session.resetMetaData(forUUID: NSUUID(UUIDString: self.uploadFile1UUID.stringValue)!)
+
+        self.singleDownload.append() { (downloadedFile:NSURL, downloadedFileAttr: SMSyncAttributes) in
+            XCTAssert(downloadedFileAttr.uuid.UUIDString == testFile.uuidString)
+            let filesAreTheSame = SMFiles.compareFiles(file1: testFile.url, file2: downloadedFile)
+            XCTAssert(filesAreTheSame)
+            numberDownloads += 1
+            expectations.singleDownloadExpectation.fulfill()
+        }
+        
+        self.shouldSaveDownloads.append() { downloadedFiles, ack in
+            XCTAssert(numberDownloads == 1)
+            XCTAssert(downloadedFiles.count == 1)
+            let (_, _) = downloadedFiles[0]
+            expectations.allDownloadsCompleteExpectation.fulfill()
+            ack()
+        }
+        
+        self.idleCallbacks.append() {
+            expectations.idleExpectation.fulfill()
+            complete?()
+        }
+        
+        // Force the check for downloads.
+        SMSyncControl.session.nextSyncOperation()
+    }
     
-    // Test the following two when having redeemed *no* invitations. (i.e., the minimum capabilities in a sharing invitation is for downloading).
+    func uploadDeletion(testFile:TestFile, expectation:UploadDeletionExpectations,
+        complete:(()->())?=nil) {
+
+        try! SMSyncServer.session.deleteFile(testFile.uuid)
+        
+        self.deletionCallbacks.append() { uuids in
+            XCTAssert(uuids.count == 1)
+            XCTAssert(uuids[0].UUIDString == testFile.uuidString)
+            expectation.deletionExpectation.fulfill()
+        }
+        
+        // The .Idle callback gets called first
+        self.idleCallbacks.append() {
+            expectation.idleExpectation.fulfill()
+        }
+        
+        // Followed by the commit complete.
+        self.commitCompleteCallbacks.append() { numberDeletions in
+            Log.msg("commitCompleteCallbacks: deleteFiles")
+            XCTAssert(numberDeletions == 1)
+            
+            let fileAttr = SMSyncServer.session.localFileStatus(testFile.uuid)
+            XCTAssert(fileAttr != nil)
+            XCTAssert(fileAttr!.deleted!)
+            
+            expectation.commitCompleteExpectation.fulfill()
+            complete?()
+        }
+    
+        try! SMSyncServer.session.commit()
+    }
+    
+    func downloadDeletion(testFile:TestFile, expectation:DownloadDeletionExpectations,
+        complete:(()->())?=nil) {
+        
+        SMSyncServer.session.resetMetaData(forUUID: testFile.uuid, resetType: .Undelete)
+
+        self.shouldDoDeletions.append() { deletions, acknowledgement in
+            XCTAssert(deletions.count == 1)
+            let attr = deletions[0]
+
+            XCTAssert(attr.uuid.UUIDString == testFile.uuidString)
+            
+            let fileAttr = SMSyncServer.session.localFileStatus(testFile.uuid)
+            XCTAssert(fileAttr != nil)
+            XCTAssert(fileAttr!.deleted!)
+            
+            expectation.clientShouldDeleteFilesExpectation.fulfill()
+            acknowledgement()
+        }
+
+        self.idleCallbacks.append() {
+            expectation.idle.fulfill()
+            complete?()
+        }
+        
+        SMSyncControl.session.nextSyncOperation()
+    }
+    
+    // These two tests have no meaning. In order for a sharing user to sign in to the system, they must have some capabilities-- i.e., they must have at least Downloader capabilities. Thus, the following two tests cannot be carried out.
+    /*
     func testThatFileDownloadByUnauthorizedSharingUserFails() {
     }
 
     func testThatDownloadDeletionByUnauthorizedSharingUserFails() {
     }
+    */
+    
+    // ***** For the rest: Must be signed in as sharing user.
+    // 1) Startup the app in Xcode, not doing a test.
+    // 2) Sign out of owning user. 
+    // 3) Sign in as Facebook user.
     
     func testThatFileDownloadByDownloadSharingUserWorks() {
         // Redeem Download invitation first.
+        let downloadInvitation = 0
+        let invitationCode = self.downloadingInvitations[downloadInvitation].stringValue
+        
+        let expectations = DownloadFileExpectations(fromTestClass: self)
+        
+        self.extraServerResponseTime = 30
+        
+        self.createUploadFiles(initial:false)
+        
+        self.waitUntilSyncServerUserSignin() {
+            SMSyncServerUser.session.redeemSharingInvitation(invitationCode: invitationCode) { (linkedOwningUserId, error) in
+                XCTAssert(linkedOwningUserId == nil)
+                XCTAssert(error != nil)
+                
+                self.downloadFile(self.uploadFile1, expectations: expectations)
+            }
+        }
+        
+        self.waitForExpectations()
     }
     
     func testThatDownloadDeletionByDownloadSharingUserWorks() {
         // Redeem Download invitation first.
+        let downloadInvitation = 1
+        let invitationCode = self.downloadingInvitations[downloadInvitation].stringValue
+        
+        let expectations = DownloadDeletionExpectations(fromTestClass: self)
+        
+        self.extraServerResponseTime = 30
+        
+        self.createUploadFiles(initial:false)
+        
+        self.waitUntilSyncServerUserSignin() {
+            SMSyncServerUser.session.redeemSharingInvitation(invitationCode: invitationCode) { (linkedOwningUserId, error) in
+                XCTAssert(linkedOwningUserId == nil)
+                XCTAssert(error != nil)
+
+                self.downloadDeletion(self.uploadFile2, expectation: expectations)
+            }
+        }
+        
+        self.waitForExpectations()
     }
     
     func testThatDownloadDeletionByUploadSharingUserWorks() {
@@ -173,10 +388,30 @@ class SharingUserOperations: BaseClass {
     
     func testThatFileUploadByDownloadingSharingUserFails() {
         // Redeem Download invitation first.
+        let downloadInvitation = 2
+        let invitationCode = self.downloadingInvitations[downloadInvitation].stringValue
+        
+        let expectations = UploadFileExpectations(fromTestClass: self)
+        
+        self.extraServerResponseTime = 30
+        
+        let testFile = TestBasics.session.createTestFile("FileUploadByDownloadingSharingUser")
+        
+        self.waitUntilSyncServerUserSignin() {
+            SMSyncServerUser.session.redeemSharingInvitation(invitationCode: invitationCode) { (linkedOwningUserId, error) in
+                XCTAssert(linkedOwningUserId == nil)
+                XCTAssert(error != nil)
+
+                self.uploadFile(testFile, expectations: expectations, failureExpected: true)
+            }
+        }
+        
+        self.waitForExpectations()
     }
     
     func testThatUploadDeletionByDownloadingSharingUserFails() {
         // Redeem Download invitation first.
+        let downloadInvitation = 3
     }
     
     func testThatFileUploadByUploadSharingUserWorks() {
@@ -199,6 +434,8 @@ class SharingUserOperations: BaseClass {
     
     func testThatInvitationByDownloadingSharingUserFails() {
         // Redeem Download invitation first.
+        let downloadInvitation = 4
+
     }
     
     func testThatInvitationByUploadSharingUserFails() {
